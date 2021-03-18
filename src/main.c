cdd98e4b10b7a09f7e26b26dc4b8ff20eb031eb8
Alexandre Miguel <aleronupe@gmail.com>
2021-03-18 16:54:05 -0300

    Print temperature

--- src/main.c
+++ src/main.c
@@ -3,8 +3,20 @@
 #include <fcntl.h>   //Used for UART
 #include <termios.h> //Used for UART
 #include <string.h>
+#include <linux/i2c-dev.h> //Used for I2C
+#include <sys/ioctl.h>     //Used for I2C
 #include "../inc/crc16.h"
 #include "../inc/bme280.h"
+
+/* Structure that contains identifier details used in example */
+struct identifier
+{
+    /* Variable to hold device address */
+    uint8_t dev_addr;
+
+    /* Variable that contains file descriptor */
+    int8_t fd;
+};
 
 void monta_options_uart(int uart0_filestream)
 {
@@ -16,6 +28,33 @@ void monta_options_uart(int uart0_filestream)
     tcgetattr(uart0_filestream, &options);
     tcflush(uart0_filestream, TCIFLUSH);
     tcsetattr(uart0_filestream, TCSANOW, &options);
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] reg_addr       : Register address.
+ *  @param[out] data          : Pointer to the data buffer to store the read data.
+ *  @param[in] len            : No of bytes to read.
+ *  @param[in, out] intf_ptr  : Void pointer that can enable the linking of descriptors
+ *                                  for interface related call backs.
+ *
+ *  @return Status of execution
+ *
+ *  @retval 0 -> Success
+ *  @retval > 0 -> Failure Info
+ *
+ */
+int8_t user_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr)
+{
+    struct identifier id;
+
+    id = *((struct identifier *)intf_ptr);
+
+    write(id.fd, &reg_addr, 1);
+    read(id.fd, data, len);
+
+    return 0;
 }
 
 int main(int argc, const char *argv[])
@@ -49,8 +88,8 @@ int main(int argc, const char *argv[])
 
         unsigned char rx_buffer[256];
         int rx_length = read(uart0_filestream, (void *)rx_buffer, 255);
-        float temperatura;
-        memcpy(&temperatura, &rx_buffer[3], 4);
+        float temp_TI;
+        memcpy(&temp_TI, &rx_buffer[3], 4);
         if (rx_length > 0)
         {
             rx_buffer[rx_length] = '\0';
@@ -62,11 +101,20 @@ int main(int argc, const char *argv[])
 
             crc_calc = calcula_CRC(rx_buffer, 7);
             printf("crc calculado: %d\n", crc_calc);
+
+            memcpy(&temp_TI, &rx_buffer[3], 4);
+            printf("temp_TI: %f\n", temp_TI);
         }
     }
     close(uart0_filestream);
 
     ////////////////////// I2C ///////////////////
+    struct bme280_dev dev;
+
+    struct identifier id;
+
+    /* Variable to define the result */
+    int8_t rslt = BME280_OK;
 
     return 0;
 }
