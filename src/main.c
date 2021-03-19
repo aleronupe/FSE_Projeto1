3af62717cf4ac916dbcbf2e81099e6b038fcff40
Alexandre Miguel <aleronupe@gmail.com>
2021-03-19 01:09:51 -0300

    Separate UART in functions

--- src/main.c
+++ src/main.c
@@ -6,9 +6,17 @@
 #include <linux/i2c-dev.h> //Used for I2C
 #include <sys/ioctl.h>     //Used for I2C
 #include <stdlib.h>
+
 #include "../inc/crc16.h"
 #include "../inc/bme280.h"
 #include "../inc/i2c.h"
+
+void abre_uart(int *uart0_filestream)
+{
+    *uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (*uart0_filestream == -1)
+        printf("Erro - Não foi possível iniciar a UART.\n");
+}
 
 void monta_uart(int uart0_filestream)
 {
@@ -22,70 +30,83 @@ void monta_uart(int uart0_filestream)
     tcsetattr(uart0_filestream, TCSANOW, &options);
 }
 
+int confere_crc(unsigned char *rx_buffer, int rx_length)
+{
+    if (rx_length > 0)
+    {
+        short crc_calc, crc_rec;
+
+        memcpy(&crc_rec, rx_buffer[7], 2);
+        printf("crc recebido: %d\n", crc_rec);
+
+        crc_calc = calcula_CRC(rx_buffer, 7);
+        printf("crc calculado: %d\n", crc_calc);
+
+        if (crc_calc == crc_rec)
+            return 1;
+        else
+            return 0;
+    }
+    else
+        return 0;
+}
+
+void le_temperatura(int uart0_filestream, unsigned char sub_cod, float *temp)
+{
+    unsigned char msg_temp[20] = {0x01, 0x23, sub_cod, 0x0, 0x8, 0x4, 0x0};
+    short crc_temp = calcula_CRC(msg_temp, 7);
+    memcpy(&msg_temp[7], (const void *)&crc_temp, 2);
+
+    int count = write(uart0_filestream, &msg_temp[0], 9);
+    if (count < 0)
+        printf("UART TX error\n");
+
+    usleep(100000);
+    // sleep(1);
+
+    int rx_length = 0, out_crc = 0;
+    unsigned char rx_buffer[256];
+    while(out_crc != 1){
+        out_crc = confere_crc(&rx_buffer, rx_length);
+        rx_length = read(uart0_filestream, (void *)rx_buffer, 255);
+    }
+    
+    memcpy(temp, &rx_buffer[3], 4);
+}
+
 int main(int argc, const char *argv[])
 {
 
     ////////////// UART///////////////////////
     int uart0_filestream = -1;
-    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (uart0_filestream == -1)
-        printf("Erro - Não foi possível iniciar a UART.\n");
+    float temp_int, temp_ref;
 
     monta_uart(uart0_filestream);
-
-    unsigned char msg_TI[20] = {0x01, 0x23, 0xC1, 0x0, 0x8, 0x4, 0x0};
-    short crc_TI = calcula_CRC(msg_TI, 7);
-    memcpy(&msg_TI[7], (const void *)&crc_TI, 2);
-
-    unsigned char msg_TP[20] = {0x01, 0x23, 0xC2, 0x0, 0x8, 0x4, 0x0};
-    short crc_TP = calcula_CRC(msg_TP, 7);
-    memcpy(&msg_TP[7], (const void *)&crc_TP, 2);
+    abre_uart(&uart0_filestream);
 
     if (uart0_filestream != -1)
     {
-        int count = write(uart0_filestream, &msg_TI[0], 9);
-        if (count < 0)
-            printf("UART TX error\n");
-        else
-            printf("escrito.\n");
-
-        sleep(1);
-
-        unsigned char rx_buffer[256];
-        int rx_length = read(uart0_filestream, (void *)rx_buffer, 255);
-        float temp_TI;
-        memcpy(&temp_TI, &rx_buffer[3], 4);
-        if (rx_length > 0)
-        {
-            short crc_calc, crc_rec;
-
-            memcpy(&crc_rec, &rx_buffer[7], 2);
-            printf("crc recebido: %d\n", crc_rec);
-
-            crc_calc = calcula_CRC(rx_buffer, 7);
-            printf("crc calculado: %d\n", crc_calc);
-
-            memcpy(&temp_TI, &rx_buffer[3], 4);
-            printf("temp_TI: %f\n", temp_TI);
-        }
+        le_temperatura(uart0_filestream, 0xC1, &temp_int);
+        le_temperatura(uart0_filestream, 0xC2, &temp_ref);
     }
     close(uart0_filestream);
 
     ////////////////////// I2C ///////////////////
 
-    double temp_ambiente = 0.0;
+    double temp_amb = 0.0;
 
     struct bme280_dev dev;
     struct identifier id;
 
     monta_i2c(&dev, &id);
     abre_i2c(&dev, &id);
-    le_temp_i2c(&dev, &temp_ambiente);
-    printf("temperatura ambiente na main: %lf\n", temp_ambiente);
+    le_temp_i2c(&dev, &temp_amb);
+    printf("temperatura ambiente na main: %lf\n", temp_amb);
     printf("Agora com módulos :)");
 
     close(id.fd);
 
+    ////////////////////// GPIO ///////////////////
+
     return 0;
 }
-
