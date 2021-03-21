c56b89425b9e78443397bafcc9fb12a19d895930
Alexandre Miguel <aleronupe@gmail.com>
2021-03-21 17:02:20 -0300

    Create new menu

--- src/my_panel.c
+++ src/my_panel.c
@@ -1,43 +1,58 @@
-
 #include "my_panel.h"
- 
-void print_panel(Arg_Struct *main_struct) {
-     
+
+void print_panel(Arg_Struct *main_struct)
+{
+
     WINDOW *w;
-    char list[5][7] = { "One", "Two", "Three", "Four", "Five" };
-    char item[7];
-    int ch, i = 0, width = 7;
- 
-    initscr(); // initialize Ncurses
-    w = newwin( 10, 12, 1, 1 ); // create a new window
-    box( w, 0, 0 ); // sets default borders for the window
-     
-// now print all the menu items and highlight the first one
-    for( i=0; i<5; i++ ) {
-        if( i == 0 ) 
-            wattron( w, A_STANDOUT ); // highlights the first item.
-        else
-            wattroff( w, A_STANDOUT );
-        sprintf(item, "%-7s",  list[i]);
-        mvwprintw( w, i+1, 2, "%s", item );
+    int ch, i = 0, width = 30;
+    float temp_ref;
+
+    if (main_struct->flag_insert_temp)
+    {
+        temp_ref = main_struct->temp_ref_user;
     }
- 
-    wrefresh( w ); // update the terminal screen
- 
+    else
+    {
+        temp_ref = main_struct->temp_ref_pot;
+    }
+
+    initscr();               // initialize Ncurses
+    w = newwin(50, 3, 1, 1); // create a new window
+    box(w, 0, 0);            // sets default borders for the window
+
+    wattroff(w, A_STANDOUT);
+    sprintf(item, "TI: %f", main_struct->temp_int);
+    mvwprintw(w, 1, 2, "%s", item);
+
+    wattroff(w, A_STANDOUT);
+    sprintf(item, "TE: %lf", main_struct->temp_ext);
+    mvwprintw(w, 2, 2, "%s", item);
+
+    wattroff(w, A_STANDOUT);
+    sprintf(item, "TR: %f", temp_ref);
+    mvwprintw(w, 3, 2, "%s", item);
+
+    wrefresh(w); // update the terminal screen
+
     i = 0;
-    noecho(); // disable echoing of characters on the screen
-    keypad( w, TRUE ); // enable keyboard input for the window.
-    curs_set( 0 ); // hide the default screen cursor.
-     
-       // get the input
-    while(( ch = wgetch(w)) != 'q'){ 
-         
-                // right pad with spaces to make the items appear with even width.
-            sprintf(item, "%-7s",  list[i]); 
-            mvwprintw( w, i+1, 2, "%s", item ); 
-             wrefresh( w ); // update the terminal screen
+    noecho();        // disable echoing of characters on the screen
+    keypad(w, TRUE); // enable keyboard input for the window.
+    curs_set(0);     // hide the default screen cursor.
+
+    // get the input
+    while (main_struct->flag_run)
+    {
+        if (main_struct->flag_insert_temp)
+        {
+            temp_ref = main_struct->temp_ref_user;
+        }
+        else
+        {
+            temp_ref = main_struct->temp_ref_pot;
+        }
+        wrefresh(w); // update the terminal screen
     }
- 
-    delwin( w );
+
+    delwin(w);
     endwin();
 }
