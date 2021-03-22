0657e9666378b87c9bbe06fc590069168bdbe7df
Alexandre Miguel <aleronupe@gmail.com>
2021-03-22 12:17:25 -0300

    Remove color

--- src/menu.c
+++ src/menu.c
@@ -8,10 +8,6 @@ void createImprimeDadosWindow()
 {
 
 	windowImprimeDados = newwin(5, 20, 1, 1);
-	start_color();
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_BLACK);
 	box(windowImprimeDados, 0, 0);
 	wrefresh(windowImprimeDados);
 }
@@ -35,17 +31,11 @@ void imprimeDados(Arg_Struct *main_struct)
 
 		wclear(windowImprimeDados);
 
-		attron(COLOR_PAIR(1));
 		mvwprintw(windowImprimeDados, 1, 1, "TI: %3.2f", main_struct->temp_int);
-		attroff(COLOR_PAIR(1));
 
-		attron(COLOR_PAIR(2));
 		mvwprintw(windowImprimeDados, 2, 1, "TE: %3.2lf", main_struct->temp_ext);
-		attroff(COLOR_PAIR(2));
 
-		attron(COLOR_PAIR(3));
 		mvwprintw(windowImprimeDados, 3, 1, "TR: %3.2f", temp_ref);
-		attroff(COLOR_PAIR(3));
 
 		wrefresh(windowImprimeDados);
 		sleep(1);
